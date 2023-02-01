606cd70656d7b281e59be486d1a5227d0d86d172
tyler-netek <tyler.netek@outlook.com>
2023-01-31 23:37:12 -0600

    new branch js script

--- git_test.js
+++ git_test.js
@@ -1,4 +1,5 @@
 let str_arr = ["Hey", ", ", "it ", "worked ", "on ", "my ", "first ", "try!"]
 str = str_arr.join('');
 let new_str = " Wait... this is a little complex."
+//Interesting...
 console.log(str + new_str);
